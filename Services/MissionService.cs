a727a68e547407dda9960bb2a3dc7e87120cd1cd
chezkyk <y0548532123@gmail.com>
2024-08-21 14:09:05 +0300

    Added functions that calculates time left and distance

--- Services/MissionService.cs
+++ Services/MissionService.cs
@@ -1,7 +1,19 @@
-﻿namespace MosadAPIServer.Services
+﻿using MosadAPIServer.Models;
+
+namespace MosadAPIServer.Services
 {
     public class MissionService
     {
+        public static double CalculateTimeLeft(Agent agent, Target target)
+        {
+            double distance = CalculateDistance(agent.Location.X,agent.Location.Y,target.Location.X,target.Location.Y);
+            double timeLeft = distance / 5;
+            return timeLeft;
 
+        }
+        public static double CalculateDistance(double xa, double ya, double xt, double yt)
+        {
+            return Math.Sqrt(Math.Pow(xt - xa, 2) + Math.Pow(yt - ya, 2));
+        }
     }
 }
