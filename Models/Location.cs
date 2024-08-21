eacae751ab08e5c0ed3a5c3744a4d611f1cc77a1
chezkyk <y0548532123@gmail.com>
2024-08-21 14:00:44 +0300

    update prpitise from float to int

--- Models/Location.cs
+++ Models/Location.cs
@@ -2,7 +2,7 @@
 {
     public class Location
     {
-        public float X { get; set; }
-        public float Y { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
     }
 }
