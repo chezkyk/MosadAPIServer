b53fa0749fef5ab443982f07e341aaf873189c06
chezkyk <y0548532123@gmail.com>
2024-08-21 15:55:05 +0300

    Added Create Agent and Get All Agents

--- Controllers/AgentsController.cs
+++ Controllers/AgentsController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MosadAPIServer.Models;
+using MosadAPIServer.Services;
 
 namespace MosadAPIServer.Controllers
 {
@@ -7,13 +9,36 @@ namespace MosadAPIServer.Controllers
     [ApiController]
     public class AgentsController : ControllerBase
     {
-        private readonly AgentsController _context;
+        private readonly MosadDbContext _context;
 
-        public AgentsController(AgentsController context)
+        public AgentsController(MosadDbContext context)
         {
             this._context = context;
         }
-        
+        //--Create Agent--
+        [HttpPost]
+        public async Task<IActionResult> CreateAgent(Agent agent)
+        {
+            
+            _context.Agents.Add(agent);
+            await _context.SaveChangesAsync();
+            return StatusCode(
+            StatusCodes.Status201Created,
+            new { agent = agent.Id });
+        }
+        //--Get All Agents
+        [HttpGet]
+        public IActionResult GetAllAttacks()
+        {
+
+            return StatusCode(
+                StatusCodes.Status200OK,
+                new
+                {
+                    attacks = _context.Agents.ToArray()
+                }
+            );
+        }
 
     }
 }
