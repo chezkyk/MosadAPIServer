6a45298f81772ba3996a6b7dfbd2f5b3cc487aec
chezkyk <y0548532123@gmail.com>
2024-08-26 18:16:39 +0300

    תשתית של MiddelWere

---
+++ Middleware/Global/GlobalLoginMiddleware.cs
@@ -0,0 +1,20 @@
+﻿namespace MosadAPIServer.Middleware.Global
+{
+    public class GlobalLoginMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public GlobalLoginMiddleware(RequestDelegate next)
+        {
+            this._next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var request = context.Request;
+            Console.WriteLine($"Got Reqest to server: {request.Method}{request.Path}" +
+                              $"From IP: {request.HttpContext.Connection.RemoteIpAddress}");
+            await this._next(context);
+        }
+    }
+}
