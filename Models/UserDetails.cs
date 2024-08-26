b860d426b238c44f6565f7f9e340b824f0fc3364
chezkyk <y0548532123@gmail.com>
2024-08-26 18:17:28 +0300

    הורדתי את התכונה סיסמא לאחר ששמתי לב שלא צריך

--- Models/UserDetails.cs
+++ Models/UserDetails.cs
@@ -7,6 +7,5 @@ namespace MosadAPIServer.Models
         [Key]
         public int? Id { get; set; }
         public string Username { get; set; }
-        public string Password { get; set; }
     }
 }
