b113b807ee159a2d610182c5702ce3850645a4a7
chezkyk <y0548532123@gmail.com>
2024-08-26 18:17:01 +0300

    middelwere ספציפי ל LOGIN

---
+++ Middleware/Login/LoginMiddleware.cs
@@ -0,0 +1,19 @@
+﻿namespace MosadAPIServer.Middleware.Login
+{
+    public class LoginMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public LoginMiddleware(RequestDelegate next)
+        {
+            this._next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var request = context.Request;
+            Console.WriteLine("Inside the login middleware");
+            await this._next(context);
+        }
+    }
+}
